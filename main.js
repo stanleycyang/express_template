088bd79356df1184d11d78b6ae2b700f80b6be4f
stanleycyang <stanley@stanleycyang.com>
2015-09-05 18:46:22 -0700

    Home page created

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
 import express from 'express';
+import path from 'path';
+import favicon from 'serve-favicon';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 
-const app = express();
+import home from './config/routes/index';
+
+let app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'app/views'));
+app.set('view engine', 'jade');
+
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', home);
+
+app.use( (req, res, next) => {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 export default app;
