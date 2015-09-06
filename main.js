798505978f5ef9813ccbf081904c8536e01ea0d6
stanleycyang <stanley@stanleycyang.com>
2015-09-05 19:07:03 -0700

    Server side basics ES6ed up

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ let app = express();
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'jade');
 
+// Uncomment when favicon is included
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -22,10 +23,35 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', home);
 
+// catch 404 and forward to error handler
 app.use( (req, res, next) => {
   let err = new Error('Not Found');
   err.status = 404;
   next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use( (err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use( (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 export default app;
